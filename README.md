9dce2afb7a864e2550a9b8685f33679767521f03
Tony de Faria <tonydefaria@iigethr.com>
2020-01-24 10:27:02 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# IIGETHR
+
+We design and develop digital products using brand strategy, creativity and technology to define and deliver ideas.
---
+++ components/copyright.js
@@ -0,0 +1,18 @@
+/* Copyright */
+
+class Copyright extends React.Component {
+
+  render () {
+    return (
+      <div className="copyright">
+        <div className="copyright-box">
+          <div className="copyright-row">
+            <p className="mono font-s purple-900-cl text-center">Copyright © 2020 IIGETHR Ltd. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Copyright
